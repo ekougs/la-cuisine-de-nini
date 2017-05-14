50eef9118205c43997b2c0a5ffc15eea11616a22
ekougs <ekougs@gmail.com>
2017-05-14 20:27:07 +0000

    refactor rules combination

--- la_cuisine_de_nini/choice/choice_of_day.py
+++ la_cuisine_de_nini/choice/choice_of_day.py
@@ -8,6 +8,19 @@ class Rule(object):
         raise NotImplementedError
 
 
+class AndRule(Rule):
+    def __init__(self, *rules):
+        self.rules = rules
+
+    def get_indices_to_recompute(self, dishes):
+        indices_to_recompute = []
+        for rule in self.rules:
+            rule_indices_to_recompute = rule.get_indices_to_recompute(dishes)
+            if rule_indices_to_recompute is not None:
+                indices_to_recompute.extend(rule_indices_to_recompute)
+        return list(sorted(set(indices_to_recompute)))
+
+
 class LunchRule(Rule):
     def get_indices_to_recompute(self, dishes):
         if not dishes[0]["lunch"]:
@@ -38,23 +51,22 @@ def _check_rule(rule, indices_to_compute, chosen_dishes):
     return indices_to_compute
 
 
-def _compute_potential_dishes(dishes, rules, indices_to_compute, chosen_dishes):
+def _compute_potential_dishes(dishes, composed_rule, indices_to_compute, chosen_dishes):
     for index_to_compute in indices_to_compute:
         chosen_dish = _get_random_dish(dishes)
         if index_to_compute < len(chosen_dishes):
             chosen_dishes[index_to_compute] = chosen_dish
         else:
             chosen_dishes.append(chosen_dish)
-    indices_to_compute = []
-    for rule in rules:
-        indices_to_compute = _check_rule(rule, indices_to_compute, chosen_dishes)
-    return chosen_dishes, indices_to_compute
+    return chosen_dishes, composed_rule.get_indices_to_recompute(chosen_dishes)
 
 
 def get_dishes_of_day(get_dishes_fn=get_dishes, rules=_rules):
     dishes = get_dishes_fn()
     chosen_dishes = []
     indices_to_compute = [0, 1]
+    composed_rule = AndRule(*rules)
     while len(indices_to_compute) > 0:
-        chosen_dishes, indices_to_compute = _compute_potential_dishes(dishes, rules, indices_to_compute, chosen_dishes)
+        chosen_dishes, indices_to_compute = \
+            _compute_potential_dishes(dishes, composed_rule, indices_to_compute, chosen_dishes)
     return chosen_dishes
